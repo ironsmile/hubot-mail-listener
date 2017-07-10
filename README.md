3a42d113389b62883022f8e90c3a42296fe439df
Doychin Atanasov <ironsmile@gmail.com>
2017-07-10 12:03:30 +0300

    Create a fork of email-listener

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# hubot-mail-listener
+# hubot-mail-listener-ng
 
 hubot-mail-listener adds an **IMAP** listener plugin to hubot that reports
 unread or incoming new emails (sender, subject, date, text message).
 
-Message is put in as a slack message attachment
+## Why Fork?
 
-![demo](https://raw.githubusercontent.com/gangstead/hubot-mail-listener/master/demo.png)
+This version is a fork of [hubot-mail-listener](https://github.com/gangstead/hubot-mail-listener)
+which is posting emails a Slack attachments. But not everyone uses Slack. For this reason this fork
+uses plain markdown-formatted messages.
 
 ## Installation
 
@@ -14,7 +16,7 @@ dependency.
 
 ```javascript
 "dependencies": {
-  "hubot-mail-listener": ">= 0.0.1",
+  "hubot-mail-listener-ng": ">= 1.2.0",
   ...
 }
 ```
@@ -23,18 +25,18 @@ dependency.
 
 The following variables are required to let the script work:
 
-* `HUBOT_MAIL_LISTENER_ROOMS`, comma separated list of rooms where incoming emails should be posted
-* `HUBOT_MAIL_LISTENER_USERNAME`, username
-* `HUBOT_MAIL_LISTENER_PASSWORD`, password
-* `HUBOT_MAIL_LISTENER_HOST`, mail host
+* `HUBOT_MAIL_LISTENER_NG_ROOMS`, comma separated list of rooms where incoming emails should be posted
+* `HUBOT_MAIL_LISTENER_NG_USERNAME`, username
+* `HUBOT_MAIL_LISTENER_NG_PASSWORD`, password
+* `HUBOT_MAIL_LISTENER_NG_HOST`, mail host
 
 The following variables are optional:
 
-* `HUBOT_MAIL_LISTENER_PORT`, mail host port, default to `993`
-* `HUBOT_MAIL_LISTENER_SECURE`, whether to use secure connection, default to `true`
-* `HUBOT_MAIL_LISTENER_MAILBOX`, mail box to monitor, default to `INBOX`
-* `HUBOT_MAIL_LISTENER_MARK_SEEN`, whether to mark seen email as read, default to `true`
-* `HUBOT_MAIL_LISTENER_FETCH_UNREAD`, whether to fetch unread emails on start, default to `true`
+* `HUBOT_MAIL_LISTENER_NG_PORT`, mail host port, default to `993`
+* `HUBOT_MAIL_LISTENER_NG_SECURE`, whether to use secure connection, default to `true`
+* `HUBOT_MAIL_LISTENER_NG_MAILBOX`, mail box to monitor, default to `INBOX`
+* `HUBOT_MAIL_LISTENER_NG_MARK_SEEN`, whether to mark seen email as read, default to `true`
+* `HUBOT_MAIL_LISTENER_NG_FETCH_UNREAD`, whether to fetch unread emails on start, default to `true`
 
 ## See Also
 
--- src/mail-listener.js
+++ src/mail-listener.js
@@ -5,37 +5,37 @@ const MailListener = require('mail-listener2');
 const moment = require('moment');
 
 const config = {
-  rooms: (process.env.HUBOT_MAIL_LISTENER_ROOMS || '').split(','),
-  username: process.env.HUBOT_MAIL_LISTENER_USERNAME,
-  password: process.env.HUBOT_MAIL_LISTENER_PASSWORD,
-  host: process.env.HUBOT_MAIL_LISTENER_HOST,
-  port: process.env.HUBOT_MAIL_LISTENER_PORT || 993,
-  tls: _.get(process.env, 'HUBOT_MAIL_LISTENER_SECURE', true),
+  rooms: (process.env.HUBOT_MAIL_LISTENER_NG_ROOMS || '').split(','),
+  username: process.env.HUBOT_MAIL_LISTENER_NG_USERNAME,
+  password: process.env.HUBOT_MAIL_LISTENER_NG_PASSWORD,
+  host: process.env.HUBOT_MAIL_LISTENER_NG_HOST,
+  port: process.env.HUBOT_MAIL_LISTENER_NG_PORT || 993,
+  tls: _.get(process.env, 'HUBOT_MAIL_LISTENER_NG_SECURE', true),
   tlsOptions: { rejectUnauthorized: false },
-  mailbox: process.env.HUBOT_MAIL_LISTENER_MAILBOX || 'INBOX',
-  searchFilter: (process.env.HUBOT_MAIL_LISTENER_SEARCH_FILTER || '').split(','),
-  markSeen: _.get(process.env, 'HUBOT_MAIL_LISTENER_MARK_SEEN', true),
-  fetchUnreadOnStart: process.env.HUBOT_MAIL_LISTENER_FETCH_UNREAD
+  mailbox: process.env.HUBOT_MAIL_LISTENER_NG_MAILBOX || 'INBOX',
+  searchFilter: (process.env.HUBOT_MAIL_LISTENER_NG_SEARCH_FILTER || '').split(','),
+  markSeen: _.get(process.env, 'HUBOT_MAIL_LISTENER_NG_MARK_SEEN', true),
+  fetchUnreadOnStart: process.env.HUBOT_MAIL_LISTENER_NG_FETCH_UNREAD
 };
 
 module.exports = (robot) => {
   if (config.rooms[0].length <= 0) {
-    robot.logger.error('Please set the HUBOT_MAIL_LISTENER_ROOMS environment variable.');
+    robot.logger.error('Please set the HUBOT_MAIL_LISTENER_NG_ROOMS environment variable.');
     return null;
   }
 
   if (!config.username) {
-    robot.logger.error('Please set the HUBOT_MAIL_LISTENER_USERNAME environment variable.');
+    robot.logger.error('Please set the HUBOT_MAIL_LISTENER_NG_USERNAME environment variable.');
     return null;
   }
 
   if (!config.password) {
-    robot.logger.error('Please set the HUBOT_MAIL_LISTENER_PASSWORD environment variable.');
+    robot.logger.error('Please set the HUBOT_MAIL_LISTENER_NG_PASSWORD environment variable.');
     return null;
   }
 
   if (!config.host) {
-    robot.logger.error('Please set the HUBOT_MAIL_LISTENER_HOST environment variable.');
+    robot.logger.error('Please set the HUBOT_MAIL_LISTENER_NG_HOST environment variable.');
     return null;
   }
 
